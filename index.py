96abb088299af1cf188f1c84d5b3a2bf418c51b7
niskarsh <niskarshk@gmail.com>
2020-11-22 20:38:04 +0530

    Added 3 more fields

--- index.py
+++ index.py
@@ -5,9 +5,9 @@ import os
 root = Tk()
 root.resizable(0, 0)
 
-global speedEntry, distanceEntry, time, s_label, d_label
+global speedEntry, distanceEntry, materialDensity, mediumDensity, time, s_label, d_label
 
-speedEntry, distanceEntry, time = 1, 0, 0
+speedEntry, distanceEntry, time, materialDensity, mediumDensity = 1, 0, 0, 0, 0
 
 # Adding title to main window
 root.title('MSE659A Python Project')
@@ -74,7 +74,58 @@ distance_label.grid(row=0, column=0)
 distance.grid(row=0, column=1)
 b_distance.grid(row=1, column=1, sticky=E, pady=(5, 0))
 
-distance_frame.grid(row=1, column=0, padx=10, pady=(5, 10))
+distance_frame.grid(row=1, column=0, padx=10, pady=(5, 10), sticky=E)
+
+# Material Density Frame
+def setMaterialDensity(density):
+	materialDensity = density
+	
+materialDensity_frame = Frame(control_frame)
+materialDensity_label = Label(materialDensity_frame, text="Material Density: ")
+materialDensity = Entry(materialDensity_frame, width=30)
+b_materialDensity = Button(materialDensity_frame, text="Set Density", command=lambda:setMaterialDensity(materialDensity.get()))
+
+# materialDensity.insert(0, "eg: 1x")
+
+materialDensity_label.grid(row=0, column=0)
+materialDensity.grid(row=0, column=1)
+b_materialDensity.grid(row=1, column=1, sticky=E, pady=(5, 0))
+
+materialDensity_frame.grid(row=2, column=0, padx=10, pady=(5, 10), sticky=E)
+
+# Medium Density Frame
+def setMediumDensity(density):
+	mediumDensity = density
+	
+mediumDensity_frame = Frame(control_frame)
+mediumDensity_label = Label(mediumDensity_frame, text="Medium Density: ")
+mediumDensity = Entry(mediumDensity_frame, width=30)
+b_mediumDensity = Button(mediumDensity_frame, text="Set Density", command=lambda:setMediumDensity(mediumDensity.get()))
+
+# mediumDensity.insert(0, "eg: 1x")
+
+mediumDensity_label.grid(row=0, column=0)
+mediumDensity.grid(row=0, column=1)
+b_mediumDensity.grid(row=1, column=1, sticky=E, pady=(5, 0))
+
+mediumDensity_frame.grid(row=3, column=0, padx=10, pady=(5, 10), sticky=E)
+
+# Medium Viscosity Frame
+def setMediumViscosity(viscosity):
+	mediumViscosity = viscosity
+	
+mediumViscosity_frame = Frame(control_frame)
+mediumViscosity_label = Label(mediumViscosity_frame, text="Medium Viscosity: ")
+mediumViscosity = Entry(mediumViscosity_frame, width=30)
+b_mediumViscosity = Button(mediumViscosity_frame, text="Set Viscosity", command=lambda:setMediumViscosity(mediumViscosity.get()))
+
+# mediumViscosity.insert(0, "eg: 1x")
+
+mediumViscosity_label.grid(row=0, column=0)
+mediumViscosity.grid(row=0, column=1)
+b_mediumViscosity.grid(row=1, column=1, sticky=E, pady=(5, 0))
+
+mediumViscosity_frame.grid(row=4, column=0, padx=10, pady=(5, 10), sticky=E)
 
 # Packing component frames in window
 title_frame.grid(row=0, column=0, columnspan=2, pady=(10, 5))
